3c9099dab99663faa1b7d96e3de7a64730ace145
Renato Herebia <rherebia@gmail.com>
2017-02-10 10:29:00 -0300

    Assistindo aula 3

---
+++ AluraCar/www/js/controller.js
@@ -0,0 +1,16 @@
+angular.module('starter').controller('ListagemController', function($scope) {
+  $scope.listaDeCarros = ['BMW 120i',
+  'ONIX 1.6',
+  'Fiesta 2.0',
+  'C3 1.0',
+  'Uno Fire',
+  'Sentra 2.0',
+  'Astra Sedan',
+  'Vectra 2.0 Turbo',
+  'Hilux 4x4',
+  'Montana Cabine dupla',
+  'Outlander 2.4',
+  'Fusca 1500'];
+
+  $scope.endereco = 'Rua Vergueiro, 3185';
+});
