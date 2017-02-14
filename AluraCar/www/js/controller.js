0fb3c7afc8eeb98ce1a488d874f81614ed5a2034
Renato Herebia <rherebia@gmail.com>
2017-02-14 09:20:22 -0300

    Assistindo aula 7 e 8

--- AluraCar/www/js/controller.js
+++ AluraCar/www/js/controller.js
@@ -1,17 +1,7 @@
-angular.module('starter').controller('ListagemController', function($scope) {
-  $scope.listaDeCarros = [
-    {"nome": 'BMW 120i', "preco": 70000},
-    {"nome": 'ONIX 1.6', "preco": 35000},
-    {"nome": 'Fiesta 2.0', "preco": 52000},
-    {"nome": 'C3 1.0', "preco": 22000},
-    {"nome": 'Uno Fire', "preco": 11000},
-    {"nome": 'Sentra 2.0', "preco": 53000},
-    {"nome": 'Astra Sedan', "preco": 39000},
-    {"nome": 'Vectra 2.0 Turbo', "preco": 37000},
-    {"nome": 'Hilux 4x4', "preco": 90000},
-    {"nome": 'Montana Cabine dupla', "preco": 57000},
-    {"nome": 'Outlander 2.4', "preco": 99000},
-    {"nome": 'Fusca 1500', "preco": 6000}];
+angular.module('starter').controller('ListagemController', function($scope, CarroService) {
+  CarroService.obterCarros().then(function (dados) {
+    $scope.listaDeCarros = dados;
+  });
 
   $scope.endereco = 'Rua Vergueiro, 3185';
 }).controller('CarroEscolhidoController', function ($stateParams, $scope) {
@@ -30,15 +20,34 @@ angular.module('starter').controller('ListagemController', function($scope) {
       $scope.carroEscolhido.preco = $scope.carroEscolhido.preco - acessorio.preco;
     }
   };
-}).controller('FinalizarPedidoController', function($stateParams, $scope, $ionicPopup, $state) {
+}).controller('FinalizarPedidoController', function($stateParams, $scope, $ionicPopup, $state, CarroService) {
   $scope.carroFinalizado = angular.fromJson($stateParams.carro);
 
+  $scope.pedido = {};
+
   $scope.finalizarPedido = function () {
-    $ionicPopup.alert({
-      title: 'Parabéns',
-      template: 'Você acaba de comprar um carro.'
-    }).then(function () {
-      $state.go('listagem');
-    });
+    var pedidoFinalizado = {
+      params: {
+        carro: $scope.carroFinalizado.nome,
+        preco: $scope.carroFinalizado.preco,
+        nome: $scope.pedido.nome,
+        endereco: $scope.pedido.endereco,
+        email: $scope.pedido.email
+      }
+    };
+
+    CarroService.salvarPedido(pedidoFinalizado).then(function (dados) {
+      $ionicPopup.alert({
+        title: 'Parabéns',
+        template: 'Você acaba de comprar um carro.'
+      }).then(function () {
+        $state.go('listagem');
+      });
+    }, function (erro) {
+      $ionicPopup.alert({
+        title: 'Deu erro',
+        template: 'Campos obrigatórios'
+      });
+    })
   };
 });
