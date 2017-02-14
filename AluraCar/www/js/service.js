dfd7e09f288fe4b56cbabc48589b7d7177546993
Renato Herebia <rherebia@gmail.com>
2017-02-14 09:24:47 -0300

    Mudando a url do servidor para http

--- AluraCar/www/js/service.js
+++ AluraCar/www/js/service.js
@@ -1,5 +1,5 @@
 angular.module('starter').service('CarroService', function ($http) {
-  var url = 'https://aluracar.herokuapp.com/';
+  var url = 'http://aluracar.herokuapp.com/';
 
   return {
     obterCarros : function () {
