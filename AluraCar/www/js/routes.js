42453c2690035fdb2b6cf8a2f0b455bd9330bd34
Renato Herebia <rherebia@gmail.com>
2017-02-15 10:57:02 -0300

    Assistindo aula 2 parte 3

--- AluraCar/www/js/routes.js
+++ AluraCar/www/js/routes.js
@@ -1,10 +1,19 @@
 angular.module('starter').config(function ($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('login');
 
-  $stateProvider.state('listagem', {
+  $stateProvider.state('app', {
+    url: '/app',
+    templateUrl: 'templates/menu.html',
+    abstract: true,
+    controller: 'MenuController'
+  }).state('app.listagem', {
     url: '/listagem',
-    templateUrl: 'templates/listagem.html',
-    controller: 'ListagemController'
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/listagem.html',
+        controller: 'ListagemController'
+      }
+    }
   }).state('carroescolhido', {
     url: '/carroescolhido/:carro',
     templateUrl: 'templates/carroescolhido.html',
